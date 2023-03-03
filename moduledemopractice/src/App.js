e6e8881cc91af9499ba245517b8121a800aeb354
15797vishal <115143001+15797vishal@users.noreply.github.com>
2023-03-03 22:33:11 +0530

    added things

---
+++ moduledemopractice/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      this is all about the new coding concept and we are watching new things in this program 
+       <p> 
+       thus, in this para we will learn the new things and whatever the concept of the para will help to learn more in this 
+       </p>
+    </div>
+  );
+}
+
+export default App;
